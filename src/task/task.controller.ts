5d8610c858e876b8dca92279aaa138b8f7eae297
Russs11 <russs.khalilov@gmail.com>
2025-04-23 23:52:38 +0300

    h23.04.2025

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Get('all')
+  findAll() {
+    return [
+      {
+        id: 1,
+        title: 'learnNestJs',
+        isComplited: false,
+      },
+      {
+        id: 2,
+        title: 'BuildApi',
+        isComplited: true,
+      },
+    ];
+  }
 }
