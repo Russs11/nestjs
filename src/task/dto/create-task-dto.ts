176524ce1db066e691f0d2d2d1d0c54903b471ac
Russs11 <russs.khalilov@gmail.com>
2025-05-03 09:02:20 +0300

    h03.05.2025

--- src/task/dto/create-task-dto.ts
+++ src/task/dto/create-task-dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateTaskDto {
   @IsString({ message: 'Название задачи должно быть строкой' })
   @IsNotEmpty()
+  @Length(2, 40, { message: 'На звание должно быть от 2 до 40 символов' })
   title: string;
 }
--- src/task/dto/update-task-dto.ts
+++ src/task/dto/update-task-dto.ts
@@ -1,7 +1,9 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class UpdateTaskDto {
   @IsString({ message: 'Название задачи должно быть строкой' })
+  @IsNotEmpty()
+  @Length(2, 40, { message: 'На звание должно быть от 2 до 40 символов' })
   title: string;
 
   @IsBoolean({ message: 'Статус должен быть булевым выражением' })
