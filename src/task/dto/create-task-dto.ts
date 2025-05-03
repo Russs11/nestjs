cf012eb1bcdf20ea47bde76ef33d9e08b4eb96b6
Russs11 <russs.khalilov@gmail.com>
2025-05-03 08:57:09 +0300

    h03.05.2025

--- src/task/dto/create-task-dto.ts
+++ src/task/dto/create-task-dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'Название задачи должно быть строкой' })
+  @IsNotEmpty()
   title: string;
 }
--- src/task/dto/update-task-dto.ts
+++ src/task/dto/update-task-dto.ts
@@ -1,4 +1,9 @@
+import { IsBoolean, IsString } from 'class-validator';
+
 export class UpdateTaskDto {
+  @IsString({ message: 'Название задачи должно быть строкой' })
   title: string;
+
+  @IsBoolean({ message: 'Статус должен быть булевым выражением' })
   isComplited: boolean;
 }
